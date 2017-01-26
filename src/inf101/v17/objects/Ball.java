d4744c57c7ece2d9528df0911b21ad282f53c0fa
Olav Gjerde <olav.gjerde@student.uib.no>
2017-01-26 19:06:54 +0100

    Endret på hit-metode

--- src/inf101/v17/objects/Ball.java
+++ src/inf101/v17/objects/Ball.java
@@ -157,13 +157,18 @@ public class Ball {
 	 * @param bounceY Y-distance the ball needs to move in order to not overlap with the object it hit
 	 */
 	public void hit(double bounceX, double bounceY) {
+		this.xPos += bounceX;
+		this.yPos += bounceY;
+
 		if (bounceX != 0 || bounceY != 0) {
-			this.xPos -= bounceX;
-			this.yPos -= bounceY;
+			deltaX -= deltaX*2;
+			deltaY -= deltaY*2;
 		}
-		else {
-			this.xPos += bounceX;
-			this.yPos += bounceY;
+		else if (bounceX != 0) {
+			deltaX -= deltaX*2;
+		}
+		else if (bounceY != 0) {
+			deltaY -= deltaY*2;
 		}
 	}
 
