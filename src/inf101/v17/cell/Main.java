a716589c0a596533495e59cc423d61923befbb02
Olav Gjerde <olav.gjerde@student.uib.no>
2017-02-02 13:05:12 +0100

    Implemented Brian's Brain with the reuse of GameOfLife-code with small modifications

--- src/inf101/v17/cell/Main.java
+++ src/inf101/v17/cell/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ICellAutomaton ca = new GameOfLife(100,100);
+		//ICellAutomaton ca = new GameOfLife(100,100);
 		//Når du er klar til å teste ut BriansBrain
 		//kan du bytte linjen over med det følgende:
-		//ICellAutomaton ca = new BriansBrain(100,100);
+		ICellAutomaton ca = new BriansBrain(100,100);
 		
 		CellAutomataGUI.run(ca);
 	}
